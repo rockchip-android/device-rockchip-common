84c1e7d8ab9fe4d023c21b05a03c78deabe7f024
Steven Chen <cw@rock-chips.com>
2017-08-16 17:47:01 +0800

    perf: Rename performance_info to package_performance
    Change-Id: I2b7a16a8fdf11c1343096b7142b4a93dd9bc2528
    Signed-off-by: Steven Chen <cw@rock-chips.com>

--- device.mk
+++ device.mk
@@ -589,7 +589,7 @@ endif
 
 ifneq ($(filter rk%, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
-        device/rockchip/common/performance_info.xml:system/etc/performance_info.xml
+        device/rockchip/common/package_performance.xml:system/etc/package_performance.xml
 endif
 
 # hdmi cec
