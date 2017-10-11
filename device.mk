ed019028e938d86b3e736c8425085d2e358f64aa
willam.wei <willam.wei@rock-chips.com>
2017-10-11 16:42:55 +0800

    widevine level 1 support
    Change-Id: I3754ca0a3a31fb8bd069eddd2009758e1609b590
    Signed-off-by: willam.wei <willam.wei@rock-chips.com>

--- device.mk
+++ device.mk
@@ -568,6 +568,20 @@ $(call inherit-product-if-exists, vendor/widevine/L1/widevine_level1.mk)
 endif
 endif
 
+#for enable optee support
+ifeq ($(strip $(PRODUCT_HAVE_OPTEE)),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.enable.optee=true
+
+ifeq ($(strip $(PRODUCT_SYSTEM_VERITY)),true)
+PRODUCT_COPY_FILES += \
+       device/rockchip/common/init.optee_verify.rc:root/init.optee.rc
+else
+PRODUCT_COPY_FILES += \
+       device/rockchip/common/init.optee.rc:root/init.optee.rc
+endif
+endif
+
 #ro.product.first_api_level indicates the first api level, device has been commercially launced on.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=23
