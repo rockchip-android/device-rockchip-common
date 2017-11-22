08c577532d51b2f95bf38ccdbdc01afeefbb3a71
huangjc <huangjc@rock-chips.com>
2017-11-22 14:00:20 +0800

    build_base.sh:replace trust with ta when build WIDEVINE_LEVEL 1 for rk322x box
    Change-Id: I3fd1db85f06fc34da6a823936b882dd2b5924708

--- build_base.sh
+++ build_base.sh
@@ -25,6 +25,8 @@ PACK_TOOL_DIR=RKTools/linux/Linux_Pack_Firmware
 IMAGE_PATH=rockdev/Image-$TARGET_PRODUCT
 export PROJECT_TOP=`gettop`
 
+WIDEVINE_LEVEL=`get_build_var BOARD_WIDEVINE_OEMCRYPTO_LEVEL`
+
 #lunch $DEVICE-$BUILD_VARIANT
 
 PLATFORM_VERSION=`get_build_var PLATFORM_VERSION`
@@ -51,6 +53,13 @@ if [ $? -eq 0 ]; then
 else
     echo "Build uboot failed!"
     exit 1
+fi
+#trust: for rk3229 box WIDEVINE_LEVEL 1 must use ta trust
+if [ "$WIDEVINE_LEVEL" = "1" ]; then
+  if [ "$DEVICE" = "rk322x_box" ]; then
+       mv u-boot/trust_with_ta.img u-boot/trust.img
+       echo "WIDEVINE_LEVEL 1 use ta trust"
+  fi
 fi
 
 # build kernel
